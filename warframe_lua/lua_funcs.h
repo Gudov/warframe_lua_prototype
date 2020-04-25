b63587f22104f000e723189fe1ebc1a6a40e4706
Gudov <dima@novator.su>
2020-04-25 23:25:59 +0700

    delete dead code

--- warframe_lua/lua_funcs.h
+++ warframe_lua/lua_funcs.h
@@ -16,9 +16,6 @@ int lua_to_token(lua_State *L);
 int lua_from_token(lua_State *L);
 int lua_get_registry(lua_State *L);
 int my_error(lua_State *L);
-int lua_http_request(lua_State *L);
-int lua_sha2(lua_State *L);
 int lua_messagebox(lua_State *L);
-int lua_init_hooks(lua_State *L);
 
 void load_lua_funcs(lua_State *L);
